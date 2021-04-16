db0d1b911727a5901888b4beeebdbc2ba8fba737
孙琦 <993473797@qq.com>
2021-04-17 00:01:06 +0800

    Update

---
+++ algorithmAndAataAtructure/算法/回溯+剪枝/52. N皇后 II.swift
@@ -0,0 +1,18 @@
+//
+//  52. N皇后 II.swift
+//  algorithmAndAataAtructure
+//
+//  Created by 孙琦 on 2021/4/16.
+//  Copyright © 2021 孙琦. All rights reserved.
+//  https://leetcode-cn.com/problems/n-queens-ii/
+/*
+ n 皇后问题 研究的是如何将 n 个皇后放置在 n×n 的棋盘上，并且使皇后彼此之间不能相互攻击。
+
+ 给你一个整数 n ，返回 n 皇后问题 不同的解决方案的数量。
+ */
+
+import Foundation
+
+func totalNQueens(_ n: Int) -> Int {
+
+}
